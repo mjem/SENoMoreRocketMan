d7f93795c7b4b95e4e93c374122470e50e07df45
Mike Elson <mike.elson@fastmail.com>
2023-10-20 03:03:47 +0200

    Linting, tidy up, proper hide of techs, earlier space elevator, fix reqs for space probe

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -3,15 +3,16 @@
 --
 
 -- Disable `tech` (sort of)
-function hide_tech(tech)
-   data.raw.technology[tech].prerequisites = {"se-teleportation"}
+local function hide_tech(tech)
+   -- data.raw.technology[tech].prerequisites = {"se-teleportation"}
+   data.raw.technology[tech].hidden = true
 end
 
 -- Remove `ingredient` from `recipe`
 -- Only works if the recipe was created using tuples for ingredients
 -- if they are given are {name=xxx, amount=yyy} this function silently fails
-function remove_ingredient(recipe, ingredient)
-   new_ingredients = {}
+local function remove_ingredient(recipe, ingredient)
+   local new_ingredients = {}
    for _, ingredient_amount in ipairs(data.raw.recipe[recipe].ingredients) do
 	  if ingredient_amount[1] ~= ingredient then
 		 table.insert(new_ingredients, ingredient_amount)
@@ -129,7 +130,7 @@ if settings.startup["senomorerocketman-earlier-spaceships"].value then
 		 {"chemical-science-pack", 1},
 		 {"se-rocket-science-pack", 1},
 		 {"space-science-pack", 1},
-		 {"se-energy-science-pack-1", 1},
+		 {"se-astronomic-science-pack-1", 1},
 	  },
 	  time = 15,
    }
@@ -151,11 +152,23 @@ end
 --
 
 if settings.startup["senomorerocketman-remove-cargo-rockets"].value == true then
+   -- Hide the relevant techs
    hide_tech("se-rocket-launch-pad")
    hide_tech("se-rocket-landing-pad")
    hide_tech("se-rocket-cargo-safety-1")
+   hide_tech("se-rocket-cargo-safety-2")
+   hide_tech("se-rocket-cargo-safety-3")
+   hide_tech("se-rocket-cargo-safety-4")
    hide_tech("se-rocket-reusability-1")
+   hide_tech("se-rocket-reusability-2")
+   hide_tech("se-rocket-reusability-3")
+   hide_tech("se-rocket-reusability-4")
    hide_tech("se-rocket-survivability-1")
+   hide_tech("se-rocket-survivability-2")
+   hide_tech("se-rocket-survivability-3")
+   hide_tech("se-rocket-survivability-4")
+   -- We still need space probe rockets to collect asteroid belt probe data for astro 4 science
+   remove_ingredient("se-space-probe-rocket", "se-cargo-rocket-section")
 end
 
 --
@@ -164,6 +177,7 @@ end
 
 if settings.startup["senomorerocketman-remove-delivery-canons"].value == true then
    hide_tech("se-delivery-cannon")
+   hide_tech("se-delivery-cannon-capsule-iridium")
 end
 
 --
@@ -202,6 +216,7 @@ end
 --
 --- Cheaper satellites
 --
+
 if settings.startup["senomorerocketman-cheaper-satellite-launches"] then
    local data_util = require("__space-exploration__.data_util")
    data_util.replace_or_add_ingredient("rocket-part", "rocket-fuel", "rocket-fuel", 1)
@@ -210,6 +225,7 @@ end
 --
 --- Earlier lithium-sulfur batteries
 --
+
 if settings.startup["senomorerocketman-earlier-lithium-sulfur-battery"] then
    data.raw.technology["kr-lithium-sulfur-battery"].prerequisites = {
 	  "production-science-pack",
@@ -232,12 +248,13 @@ end
 --
 --- Earlier space railways
 --
+
 if settings.startup["senomorerocketman-earlier-space-railway"] then
-   -- Remove holmium cable tech prereq
+   -- Remove holmium cable tech prereq to space railways
    data.raw.technology["se-space-rail"].prerequisites = {
 	  "kr-lithium-sulfur-battery",
    }
-   -- Remove energy science packs
+   -- Remove energy science packs from space railway tech
    data.raw.technology["se-space-rail"].unit = {
 	  count = 50,
 	  ingredients = {
@@ -252,11 +269,82 @@ if settings.startup["senomorerocketman-earlier-space-railway"] then
 	  time = 60,
    }
    -- Remove holmium cable from space trace recipie
+   -- (remove_ingredient needs adapting to the way these recipies are specified)
    -- remove_ingredient("se-space-rail", "se-holmium-cable")
    -- remove_ingredient("se-space-rail", "se-energy-catalogue-1")
    data.raw.recipe["se-space-rail"].ingredients = {
     { name = "rail", amount = 100},
     { name = "steel-plate", amount = 100},
+   }
+   -- Remove all material science prereq from space elevator but leave holmium and beryllium techs
+   data.raw.technology["se-space-elevator"].prerequisites = {
+	  "se-space-mechanical-laboratory",
+	  "se-holmium-cable",
+	  "se-aeroframe-pole"
+   }
+   -- Allow space elevator to be researched with the production + utility science
+   data.raw.technology["se-space-elevator"].unit = {
+	  count = 50,
+	  ingredients = {
+		 {"automation-science-pack", 1},
+		 {"logistic-science-pack", 1},
+		 {"chemical-science-pack", 1},
+		 {"se-rocket-science-pack", 1},
+		 {"space-science-pack", 1},
+		 {"production-science-pack", 1},
+		 {"utility-science-pack", 1},
+	  },
+	  time = 60,
+   }
+   -- Original construction of space elevator needs
+   --  holmium cable - energy 1, holmium
+   --  processing unit
+   --  heavy girder
+   --  heavy bearing
+   --  aeroframe pole
+   --  refined concrete
+   -- To allow the elevator to be created and used earlier we:
+   --  ease holmium cable by removing energy 1 as prerequisite
+   --  remove heavy girder and heavy bearing as they need material 1 and 2
+   --  add steel, big electric motors and low density structures to compensate
+   data.raw.technology["se-holmium-cable"].unit = {
+	  count = 20,
+	  ingredients = {
+		 {"automation-science-pack", 1},
+		 {"logistic-science-pack", 1},
+		 {"chemical-science-pack", 1},
+		 {"se-rocket-science-pack", 1},
+		 {"space-science-pack", 1},
+		 {"production-science-pack", 1},
+		 {"utility-science-pack", 1},
+	  },
+	  time = 60,
+   }
+   data.raw.technology["se-holmium-cable"].prerequisites = {
+	  "se-space-hypercooling-1",
+	  "se-space-radiation-laboratory",
+	  "se-space-laser-laboratory",
+   }
+   data.raw.recipe["se-space-elevator"].ingredients = {
+    { name = "se-holmium-cable", amount = 500},
+    { name = "processing-unit", amount = 500},
+    { name = "se-aeroframe-pole", amount = 2000},
+    { name = "refined-concrete", amount = 1000},
+    { name = "steel-plate", amount = 1000},
+    { name = "electric-engine-unit", amount = 100},
+    { name = "low-density-structure", amount = 500},
+   }
+   -- Original cable recipe needs
+   --  holmium cable
+   --  heavy girder
+   --  aeroframe pole
+   --  coal
+   -- We remove heavy girder, add heat shielding and reduce aeroframe pole from 3 to 1
+   data.raw.recipe["se-space-elevator-cable"].ingredients = {
+    { name = "se-holmium-cable", amount = 1},
+    { name = "se-aeroframe-pole", amount = 1},
+    { name = "coal", amount = 2},
+    { name = "se-heat-shielding", amount = 1},
    }
 end
 
